6c3d18d9f67c426415be89ed6367b465de237760
aaron2708 <aaronalejo2708@gmail.com>
2022-05-10 18:36:55 -0600

    Initial commit: setup hello world

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+// public: access modifier. This sets the accessibility of the class
+// class: determines the class
+// Hello: class name
+public class Hello {
+
+    // A method: a collection of statements that performs an operation (Like a function)
+    // static: is a java keyword that needs an understanding of other concepts.
+    // void: what the method returns
+    // main: the main method
+    // String[] args: the method takes string class as argument
+    public static void main(String[] args) {
+
+    }
+
+}
