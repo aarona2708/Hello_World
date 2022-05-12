5d4f21c321008678019a0706b407ae2dd33f1435
aaron2708 <aaronalejo2708@gmail.com>
2022-05-12 10:14:09 -0600

    Updated files and added primitive type examples

--- src/Hello.java
+++ src/Hello.java
@@ -30,5 +30,12 @@ public class Hello {
         // Will print 466
         System.out.println(totalNumber);
 
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
     }
 }
